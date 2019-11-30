403babcdbc927c2004c25f9c71dea7acab13da44
Elana Ryznar <eryzna@gmail.com>
2019-11-30 22:23:53 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
   resources :students, only: [:index, :show, :create, :edit, :update, :new]
+
+  resources :schoole_classes, :only [:index, :show, :create, :edit, :update, :new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
