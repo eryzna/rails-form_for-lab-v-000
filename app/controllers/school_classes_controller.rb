bc72c00f223e7f9a32f98bd94fb94545054359d2
Elana Ryznar <eryzna@gmail.com>
2019-11-30 22:58:35 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -21,6 +21,9 @@ class SchoolClassesController < ApplicationController
   end
 
   def update
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(school_class_params)
+    redirect_to school_class_path(@school_class)
   end
 
 end
