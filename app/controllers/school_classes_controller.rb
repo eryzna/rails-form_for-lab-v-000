b5641a887d6b0e0988ea3aecd94412c41fd33cce
Elana Ryznar <eryzna@gmail.com>
2019-11-30 22:42:01 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -5,6 +5,10 @@ class SchoolClassesController < ApplicationController
 
   def create
     @school_class = SchoolClass.new
+    @school_class.title = params[:title]
+    @school_class.room_number = params[:room_number]
+    @school_class.save
+    redirect_to school_class(@school_class)
   end
 
   def show
