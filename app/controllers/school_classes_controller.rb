18feaa7bd2cc8fbe5ac34a0d4e62855c90f5148b
Elana Ryznar <eryzna@gmail.com>
2019-11-30 23:45:52 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -6,7 +6,7 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
-    @school_class = SchoolClass.new(title: params[:title], room_number: params[:room_number])
+    @school_class = SchoolClass.new(params.require(:school_class).permit(:title, :room_number))
     @school_class.save
     redirect_to school_class_path(@school_class)
   end
